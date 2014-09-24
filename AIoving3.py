2dde6d506e3f1110fe0bfbe660229c01b5fbceca
morteano <morteano@stud.ntnu.no>
2014-09-24 16:19:22 +0200

    added goalTest

--- AIoving3.py
+++ AIoving3.py
@@ -6,6 +6,12 @@ def estimatedPath(pos,goalPos):
     estimatedCost=abs(pos[0]-goalPos[0])+abs(pos[1]-goalPos[1])
     return estimatedCost
 
+#checks if current position is the goal node
+def goalTest(pos, map):
+    x=pos[0]
+    y=pos[1]
+    return map[x][y]=='B'
+
 #Converts the map from text to a matrix where # gets the value -1
 def makeMap():
     available = 1
@@ -32,20 +38,18 @@ def makeMap():
 
 
 class Node(object):
-    def __init__(self, position):
-        pos = position
-        print pos
+    def __init__(self):
+        pos = None
         weight = None
         parent = None
         kids = []
 
-node2 = Node((2,1))
-node = Node((2,3))
-nodeFar = Node((2,2))
+
+node = Node()
 node.pos = (2,3)
-nodeFar.pos = (2,2)
-node.parent = nodeFar
-nodeFar.kids = node
-print makeMap()
-print node.parent.pos
-print nodeFar.kids.pos
+
+map=makeMap()
+
+print goalTest(node.pos,map)
+node.pos = (3,17)
+print goalTest(node.pos,map)
